18b52b61cf50fddb7fa65166982a53e1dc47e23e
Taha Bohra <taha.bohra72@gmail.com>
2019-10-02 17:00:03 +0200

    Adding starting project

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import Navigation  from './components/Navigation/Navigation';
+import Logo  from './components/Logo/Logo';
+import ImageLinkForm  from './components/ImageLinkForm/ImageLinkForm';
+import Rank  from './components/Rank/Rank';
+import './App.css';
+import 'tachyons';
+
+function App() {
+  return (
+    <div className="App">
+      <Navigation />
+      <Logo />
+      <Rank/>
+      <ImageLinkForm />      
+            {/*
+            <FaceRecognition />*/}
+    </div>
+  );
+}
+
+export default App;
